a499de0cbeea04fc9a3b58feb6a0ce5f24b8239b
Bhavesh Varu <bhavesh.varu97@gmail.com>
2024-01-26 17:16:42 +0530

    added meals items cards

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,21 +1,29 @@
-import classes from './page.module.css'
 import Link from 'next/link'
-export default function meals() {
+
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
+
+export default async function MealsPage() {
+  const meals = await getMeals()
+
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created
-          <span className={classes.highlight}> by you</span>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
         </h1>
         <p>
-          choose your favourite recipe and cook it yourself. It is easy and fun!
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href='/meals/share'>Share Your Favorite Recipe </Link>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   )
 }
