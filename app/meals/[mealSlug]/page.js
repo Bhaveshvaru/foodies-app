22af708f2ce97103e447a57b3138659b49eaa6bc
Bhavesh Varu <bhavesh.varu97@gmail.com>
2024-02-08 20:39:13 +0530

    added meal details page jsx and content

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,29 @@
-export default function mealsDetails() {
-  return <h1>meals details</h1>
+import { getMeal } from '@/lib/meals'
+import classes from './page.module.css'
+import Image from 'next/image'
+
+export default function mealsDetails({ params }) {
+  const meal = getMeal(params.mealSlug)
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator_email}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: '...' }}
+        ></p>
+      </main>
+    </>
+  )
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>NOT FOUND</h1>
+      <p>The Requested Page could not be found</p>
+    </main>
+  )
+}
